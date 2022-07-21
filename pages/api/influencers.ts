be0d471c58ac8026abe95d1d19956b9ffa6149f2
Sania García <58360969+saniagarciadev@users.noreply.github.com>
2022-07-21 14:13:22 +0200

    add links

--- pages/api/influencers.ts
+++ pages/api/influencers.ts
@@ -1,33 +1,33 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import path from 'path'
-const fs = require('fs');
-const Papa = require('papaparse');
+// // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// import type { NextApiRequest, NextApiResponse } from 'next'
+// import path from 'path'
+// const fs = require('fs');
+// const Papa = require('papaparse');
 
-const csvFilePath = path.join(process.cwd(), '/public/instagram_influencers.csv')
+// const csvFilePath = path.join(process.cwd(), '/public/instagram_influencers.csv')
 
-const readCSV = async (filePath: String) => {
-  const csvFile = fs.readFileSync(filePath)
-  const csvData = csvFile.toString()  
-  return new Promise(resolve => {
-    Papa.parse(csvData, {
-      header: true,
-      complete: (results: Object) => {
-        console.log('Complete', results); 
-        resolve(results);
-      }
-    });
-  });
-};
+// const readCSV = async (filePath: String) => {
+//   const csvFile = fs.readFileSync(filePath)
+//   const csvData = csvFile.toString()  
+//   return new Promise(resolve => {
+//     Papa.parse(csvData, {
+//       header: true,
+//       complete: (results: Object) => {
+//         console.log('Complete', results); 
+//         resolve(results);
+//       }
+//     });
+//   });
+// };
 
-const handler = async (
-  req: NextApiRequest,
-  res: NextApiResponse
-) => {
-  const parsed = await readCSV(csvFilePath); 
+// const handler = async (
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) => {
+//   const parsed = await readCSV(csvFilePath); 
 
-  res.status(200).json(parsed)
-}
+//   res.status(200).json(parsed)
+// }
 
-export default handler;
+// export default handler;
 
